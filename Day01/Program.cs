ddc654147d3bdc92e0658f33ba6270b8d2519838
matthewtoghill <matthew.toghill@outlook.com>
2020-12-03 18:30:24 +0000

    Remove the Reverse sort to keep data in Low to High order

--- Day01/Program.cs
+++ Day01/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.IO;
 
 namespace Day01
@@ -11,42 +10,8 @@ namespace Day01
         private static int requiredValue = 2020;
         static void Main(string[] args)
         {
-            //Part1();
             BothParts();
             Console.ReadLine();
-        }
-
-        private static void Part1()
-        {
-            // Parse input to List of integers
-            List<int> data = new List<int>();
-            foreach (var item in input)
-            {
-                int.TryParse(item, out int num);
-                data.Add(num);
-            }
-
-            bool keepGoing = true;
-
-            int x = 0, y = 0;
-
-            while (keepGoing)
-            {
-                x = data.First();
-                for (int i = 1; i < data.Count; i++)
-                {
-                    if (data[i] + x == requiredValue)
-                    {
-                        y = data[i];
-                        keepGoing = false;
-                    }
-                }
-                data.RemoveAt(0);
-            }
-
-            Console.WriteLine("Part 1:");
-            Console.WriteLine($"{x} + {y} = {x + y}");
-            Console.WriteLine($"{x} * {y} = {x * y}");
         }
 
         private static void BothParts()
@@ -59,19 +24,16 @@ namespace Day01
                 data.Add(num);
             }
 
-            // Sort High to Low
+            // Sort Low to High
             data.Sort();
-            data.Reverse();
-
-            int a = 0, b = 0, c = 0;
 
             for (int i = 0; i < data.Count; i++)
             {
-                a = data[i];
+                int a = data[i];
 
                 for (int j = 1; j < data.Count; j++)
                 {
-                    b = data[j];
+                    int b = data[j];
 
                     if (a + b == requiredValue)
                     {
@@ -82,7 +44,7 @@ namespace Day01
 
                     for (int k = 2; k < data.Count; k++)
                     {
-                        c = data[k];
+                        int c = data[k];
 
                         if (a + b + c == requiredValue)
                         {
@@ -94,8 +56,6 @@ namespace Day01
                     }
                 }
             }
-
-
         }
     }
 }
