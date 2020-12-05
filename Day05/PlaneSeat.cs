f8b85f0b48e0b3aec23f5525f877fe7287f9fbe9
matthewtoghill <matthew.toghill@outlook.com>
2020-12-05 18:06:08 +0000

    Day 5 Solved

---
+++ Day05/PlaneSeat.cs
@@ -0,0 +1,73 @@
+﻿namespace Day05
+{
+    class PlaneSeat
+    {
+        private int maxRows = 127;
+        private int maxCols = 7;
+
+        public int ID { get; private set; }
+        public int Row { get; private set; }
+        public int Column { get; private set; }
+        public string PartitionString { get; private set; }
+
+        public PlaneSeat(string partitionString)
+        {
+            PartitionString = partitionString;
+            Row = GetRow(partitionString.Substring(0, 7));
+            Column = GetColumn(partitionString.Substring(7));
+            ID = Row * 8 + Column;
+        }
+
+        private int GetRow(string frontBack)
+        {
+            int lowR = 0, highR = maxRows, result = 0;
+
+            for (int i = 0; i < frontBack.Length; i++)
+            {
+                switch (frontBack[i])
+                {
+                    case 'F':
+                        // Set highR to be the top of lower half of the lowR to highR range
+                        highR = (highR - (highR - lowR) / 2) - 1;
+                        result = highR;
+                        break;
+                    case 'B':
+                        // Set lowR to be the bottom of the upper half of the lowR to highR range
+                        lowR = (lowR + (highR - lowR) / 2) + 1;
+                        result = lowR;
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            return result;
+        }
+
+        private int GetColumn(string rightLeft)
+        {
+            int lowC = 0, highC = maxCols, result = 0;
+
+            for (int i = 0; i < rightLeft.Length; i++)
+            {
+                switch (rightLeft[i])
+                {
+                    case 'L':
+                        // Set highC to be the top of the of lower half of the lowC to highC range
+                        highC = (highC - (highC - lowC) / 2) - 1;
+                        result = highC;
+                        break;
+                    case 'R':
+                        // Set lowC to be the bottom of the upper half of the lowC to highC range
+                        lowC = (lowC + (highC - lowC) / 2) + 1;
+                        result = lowC;
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Day05/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day05
+{
+    class Program
+    {
+        private static string[] input = File.ReadAllLines(@"..\..\..\data\day05.txt");
+        static void Main(string[] args)
+        {
+            BothParts();
+            Console.ReadLine();
+        }
+
+        private static void BothParts()
+        {
+            List<PlaneSeat> planeSeats = new List<PlaneSeat>();
+
+            // Itereate through each line of the input creating PlaneSeat objects and storing in list
+            foreach (var line in input)
+            {
+                PlaneSeat seat = new PlaneSeat(line);
+                planeSeats.Add(seat);
+            }
+
+            Console.WriteLine($"\nPart 1: Max ID: {planeSeats.Max(s => s.ID)}");
+
+            // Create list of IDs
+            List<int> seatIDs = planeSeats.Select(s => s.ID).ToList();
+
+            // Get a list of any missing IDs
+            List<int> missingValues = FindMissing(seatIDs).ToList();
+            string missingIDs = string.Join(",", missingValues);
+
+            // Print list of missing IDs
+            Console.WriteLine($"\nPart 2: Missing IDs: {missingIDs}");
+        }
+
+        public static IEnumerable<int> FindMissing(IEnumerable<int> values)
+        {
+            int min = values.Min();
+            int count = values.Max() - min;
+            HashSet<int> myRange = new HashSet<int>(Enumerable.Range(min, count));
+            myRange.ExceptWith(values);
+            return myRange;
+        }
+
+    }
+}
