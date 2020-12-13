aaeb4a9e094aeb15b8560a8d283e103d23094bed
matthewtoghill <matthew.toghill@outlook.com>
2020-12-13 22:06:17 +0000

    Use extension methods in helper class

--- Day10/Program.cs
+++ Day10/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AoCHelpers;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -63,24 +64,4 @@ namespace Day10
         }
     }
 
-    public static class DictionaryExtensions
-    {
-        public static void IncrementAt<T>(this Dictionary<T, int> dictionary, T index)
-        {
-            dictionary.TryGetValue(index, out int count);
-            dictionary[index] = ++count;
-        }
-
-        public static void IncrementAt<T>(this Dictionary<T, int> dictionary, T index, int incrementAmount)
-        {
-            dictionary.TryGetValue(index, out int count);
-            dictionary[index] = count + incrementAmount;
-        }
-
-        public static void IncrementAt<T>(this Dictionary<T, long> dictionary, T index, long incrementAmount)
-        {
-            dictionary.TryGetValue(index, out long count);
-            dictionary[index] = count + incrementAmount;
-        }
-    }
 }
