f502000301181bbce854a5a443bdf362c6fcdc53
matthewtoghill <matthew.toghill@outlook.com>
2020-12-13 22:03:52 +0000

    Add AoC Helpers project

---
+++ AoCHelpers/DictionaryExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace AoCHelpers
+{
+    public static class DictionaryExtensions
+    {
+        public static void IncrementAt<T>(this Dictionary<T, int> dictionary, T index)
+        {
+            dictionary.TryGetValue(index, out int count);
+            dictionary[index] = ++count;
+        }
+
+        public static void IncrementAt<T>(this Dictionary<T, long> dictionary, T index)
+        {
+            dictionary.TryGetValue(index, out long count);
+            dictionary[index] = ++count;
+        }
+
+        public static void IncrementAt<T>(this Dictionary<T, int> dictionary, T index, int incrementAmount)
+        {
+            dictionary.TryGetValue(index, out int count);
+            dictionary[index] = count + incrementAmount;
+        }
+
+        public static void IncrementAt<T>(this Dictionary<T, long> dictionary, T index, long incrementAmount)
+        {
+            dictionary.TryGetValue(index, out long count);
+            dictionary[index] = count + incrementAmount;
+        }
+    }
+}
---
+++ AoCHelpers/StringExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace AoCHelpers
+{
+    public static class StringExtensions
+    {
+        public static string Left(this string input, int length)
+        {
+            return input != null && input.Length > length ? input.Substring(0, length) : input;
+        }
+
+        public static string Right(this string input, int length)
+        {
+            return input != null && input.Length > length ? input.Substring(input.Length - length) : input;
+        }
+
+        public static string StripOut(this string input, char character)
+        {
+            return input.Replace(character.ToString(), "");
+        }
+
+        public static string StripOut(this string input, params char[] chars)
+        {
+            foreach (char c in chars)
+            {
+                input = input.Replace(c.ToString(), "");
+            }
+            return input;
+        }
+
+        public static string StripOut(this string input, string subString)
+        {
+            return input.Replace(subString, "");
+        }
+
+        public static string SortString(string input)
+        {
+            char[] chars = input.ToCharArray();
+            Array.Sort(chars);
+            return new string(chars);
+        }
+    }
+}
