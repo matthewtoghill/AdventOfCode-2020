4f8273491ead75765be47bde49d55e4385ea2174
matthewtoghill <matthew.toghill@outlook.com>
2020-12-19 15:06:24 +0000

    Add NumericExtensions

---
+++ AoCHelpers/NumericExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AoCHelpers
+{
+    public static class NumericExtensions
+    {
+        public static bool IsBetween<T>(this T value, T min, T max) where T : IComparable<T>
+        {
+            return min.CompareTo(value) <= 0 && value.CompareTo(max) <= 0;
+        }
+    }
+}
